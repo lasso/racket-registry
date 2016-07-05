c632ee11062f1525dc23fd4b72c66ca3122a02cc
Lars Olsson <lasso@textalk.se>
2016-07-05 21:16:40 +0200

    Test more ruby versions, use private module instead of class methods

--- lib/racket/registry.rb
+++ lib/racket/registry.rb
@@ -25,12 +25,12 @@ module Racket
     # @param [String|Symbol] key
     # @return [nil]
     def forget(key)
-      self.class.forget(obj: self, key: key)
+      Helper.forget(obj: self, key: key)
     end
 
     # Removes all callbacks from the registry.
     def forget_all
-      self.class.forget_all(obj: self)
+      Helper.forget_all(obj: self)
     end
 
     # Registers a new callback in the registry. This will add a new method
@@ -43,9 +43,7 @@ module Racket
     # @param [Proc|nil] proc
     # @return [nil]
     def register(key, proc = nil, &block)
-      self.class.register(
-        obj: self, key: key, proc: proc, block: block
-      )
+      Helper.register(obj: self, key: key, proc: proc, block: block)
     end
 
     # Registers a new callback in the registry. This will add a new method
@@ -58,110 +56,93 @@ module Racket
     # @param [Proc|nil] proc
     # @return [nil]
     def register_singleton(key, proc = nil, &block)
-      self.class.register_singleton(
-        obj: self, key: key, proc: proc, block: block
-      )
+      Helper.register_singleton(obj: self, key: key, proc: proc, block: block)
     end
 
     alias singleton register_singleton
 
-    # Removes a callback from a registry.
-    #
-    # @param [Hash] options
-    # @return [nil]
-    def self.forget(options)
-      obj, methods, key = validate_existing(options)
-      raise KeyNotRegisteredError,
-            "Key #{key} is not registered" unless methods.include?(key)
-      obj.singleton_class.instance_eval do
-        @resolved.delete(key) if defined?(@resolved)
-        remove_method(key)
-      end && nil
+    # Private helper module for Racket Registry
+    module Helper
+      # Private method - do not call
+      def self.forget(options)
+        obj, methods, key = validate_existing(options)
+        raise KeyNotRegisteredError,
+              "Key #{key} is not registered" unless methods.include?(key)
+        obj.singleton_class.instance_eval do
+          @resolved.delete(key) if defined?(@resolved)
+          remove_method(key)
+        end && nil
+      end
+
+      # Private method - do not call
+      def self.forget_all(options)
+        obj, methods, = validate_existing(options)
+        obj.singleton_class.instance_eval do
+          @resolved.clear if defined?(@resolved)
+          methods.each { |meth| remove_method(meth) }
+        end && nil
+      end
+
+      # Private method - do not call
+      def self.register(options)
+        key, proc, proc_args, obj = validate_usable(options)
+        obj.define_singleton_method(key) do
+          proc.call(*proc_args)
+        end && nil
+      end
+
+      # Private method - do not call
+      def self.register_singleton(options)
+        key, proc, proc_args, obj = validate_usable(options)
+        resolved = resolved(options[:obj])
+        obj.define_singleton_method(key) do
+          return resolved[key] if resolved.key?(key)
+          resolved[key] = proc.call(*proc_args)
+        end && nil
+      end
+
+      def self.resolved(obj)
+        obj.singleton_class.instance_eval { @resolved ||= {} }
+      end
+
+      def self.validate_existing(options)
+        result = [options[:obj]]
+        result << result.first.singleton_methods
+        key = options.fetch(:key, nil)
+        result << key.to_sym if key
+        result
+      end
+
+      def self.validate_key(key, obj)
+        sym = key.to_sym
+        insp = key.inspect
+        raise KeyAlreadyRegisteredError, "Key #{insp} already registered" if
+          obj.singleton_methods.include?(sym)
+        raise InvalidKeyError, "Invalid key #{insp}" if
+          obj.public_methods.include?(sym) ||
+          /^[a-z\_]{1}[\d\w\_]*$/ !~ sym
+        sym
+      end
+
+      def self.validate_callback(proc, block)
+        return proc if proc.respond_to?(:call)
+        return block if block.respond_to?(:call)
+        raise InvalidCallbackError, 'Invalid callback'
+      end
+
+      def self.validate_usable(options)
+        obj = options[:obj]
+        key = validate_key(options[:key], obj)
+        proc = validate_callback(options[:proc], options[:block])
+        [key, proc, proc.arity.zero? ? [] : [obj], obj]
+      end
+
+      private_class_method :resolved, :validate_callback,
+                           :validate_existing, :validate_key,
+                           :validate_usable
     end
 
-    # Removes all callbacks from a registry.
-    #
-    # @param [Hash] options
-    # @return [nil]
-    def self.forget_all(options)
-      obj, methods, = validate_existing(options)
-      obj.singleton_class.instance_eval do
-        @resolved.clear if defined?(@resolved)
-        methods.each { |meth| remove_method(meth) }
-      end && nil
-    end
-
-    # Registers a new callback to a registry. This will add a new method
-    # matching +key+ to the registry that can be used both outside the
-    # registry and when registering other callbacks dependant of the
-    # current entry. Results from the callback will not be cached, meaning
-    # that the callback may return a different object every time.
-    #
-    # @param [Hash] options
-    # @return [nil]
-    def self.register(options)
-      key, proc, proc_args, obj = validate_usable(options)
-      obj.define_singleton_method(key) do
-        proc.call(*proc_args)
-      end && nil
-    end
-
-    # Registers a new callback to a registry. This will add a new method
-    # matching +key+ to the registry that can be used both outside the
-    # registry and when registering other callbacks dependant of the
-    # current entry. Results from the callback will be cached, meaning
-    # that the callback will return the same object every time.
-    #
-    # @param [Hash] options
-    # @return [nil]
-    def self.register_singleton(options)
-      key, proc, proc_args, obj = validate_usable(options)
-      resolved = resolved(options[:obj])
-      obj.define_singleton_method(key) do
-        return resolved[key] if resolved.key?(key)
-        resolved[key] = proc.call(*proc_args)
-      end && nil
-    end
-
-    def self.resolved(obj)
-      obj.singleton_class.instance_eval { @resolved ||= {} }
-    end
-
-    def self.validate_existing(options)
-      result = [options[:obj]]
-      result << result.first.singleton_methods
-      key = options.fetch(:key, nil)
-      result << key.to_sym if key
-      result
-    end
-
-    def self.validate_key(key, obj)
-      sym = key.to_sym
-      insp = key.inspect
-      raise KeyAlreadyRegisteredError, "Key #{insp} already registered" if
-        obj.singleton_methods.include?(sym)
-      raise InvalidKeyError, "Invalid key #{insp}" if
-        obj.public_methods.include?(sym) ||
-        /^[a-z\_]{1}[\d\w\_]*$/ !~ sym
-      sym
-    end
-
-    def self.validate_callback(proc, block)
-      return proc if proc.respond_to?(:call)
-      return block if block.respond_to?(:call)
-      raise InvalidCallbackError, 'Invalid callback'
-    end
-
-    def self.validate_usable(options)
-      obj = options[:obj]
-      key = validate_key(options[:key], obj)
-      proc = validate_callback(options[:proc], options[:block])
-      [key, proc, proc.arity.zero? ? [] : [obj], obj]
-    end
-
-    private_class_method :resolved, :validate_callback,
-                         :validate_existing, :validate_key,
-                         :validate_usable
+    private_constant :Helper
 
     # Exception class used when a user tries to register an
     # invalid callback.
